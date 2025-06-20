952549c7d406f71acd47deabc3bdaba7d9f6f95d
nightowl955 <gth080709@gmail.com>
2025-06-20 13:22:57 +0545

    initial changes

---
+++ script.js
@@ -0,0 +1,13 @@
+function greet(){
+   let divContent = document.getElementsByClassName("jstutor");
+   divContent[0].style.backgroundColor="red"
+   divContent[1].style.backgroundColor="orange"
+   divContent[2].style.backgroundColor="yellow"
+   divContent[3].style.backgroundColor="lime"
+   divContent[4].style.backgroundColor="blue"
+   divContent[5].style.backgroundColor="cyan"
+   divContent[6].style.backgroundColor="indigo"
+
+
+
+}
